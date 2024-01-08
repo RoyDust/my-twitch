1a7e9db82d2008907f2dc564d34e9394c1ab8d64
RoyDust <3214026782@qq.com>
2024-01-09 02:12:00 +0800

    feat: 新增zustand

---
+++ app/(browse)/(home)/_components/sidebar/index.tsx
@@ -0,0 +1,13 @@
+import React, { memo } from "react";
+import { Wrapper } from "./wrapper";
+import Toggle from "./toggle";
+
+const Sidebar = () => {
+  return (
+    <Wrapper>
+      <Toggle />
+    </Wrapper>
+  );
+};
+
+export default Sidebar;
